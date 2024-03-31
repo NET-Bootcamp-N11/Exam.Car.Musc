86aed9566c420328fbaddae7b0b10e94e7ea8cb2
Tohirjon <82634626+Tohirjon-Odilov@users.noreply.github.com>
2024-03-31 08:51:37 +0500

    form netlify serverga yuklanadigan bo'ldi

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 let footer = document.querySelector(".site-footer");
-console.log(footer);
+let clone = footer.cloneNode(footer);
+console.dir(clone.hasChildNodes(footer));
+
+console.log(window.history);
+
+window.moveTo = function () {
+	alert("Hello! I am an alert box!!");
+};
